26ab37be0821a41bd943626191d97431b53e53e9
Enrique Gonzalez <enrique@saucelabs.com>
2019-09-03 16:22:16 -0700

    create creds for all DC's

--- groovy/createSauceCredentials.groovy
+++ groovy/createSauceCredentials.groovy
@@ -7,11 +7,14 @@ import com.cloudbees.plugins.credentials.domains.Domain;
 String SAUCE_USERNAME = System.getenv("DOCKER_SAUCE_USERNAME");
 String SAUCE_ACCESS_KEY = System.getenv("DOCKER_SAUCE_ACCESS_KEY");
 
-//TODO: Make this dynamic
-String restEndpoint = "https://saucelabs.com/";
+String restEndpointUS = "https://saucelabs.com/";
+String restEndpointEU = "https://eu-central-1.saucelabs.com/";
+String restEndpointHeadless = "https://us-east-1.saucelabs.com/";
 
-// String createdCredentialId = UUID.randomUUID().toString();
-SauceCredentials sauceCredentials = new SauceCredentials(CredentialsScope.GLOBAL, "saucelabs",SAUCE_USERNAME, SAUCE_ACCESS_KEY, restEndpoint, "Created by Dockerfile");
+HashMap<String, String> restEndpoints = new HashMap<>();
+restEndpoints.put("saucelabs", restEndpointUS);
+restEndpoints.put("saucelabs-eu", restEndpointEU);
+restEndpoints.put("saucelabs-headless", restEndpointHeadless);
 
 final SystemCredentialsProvider credentialsProvider = SystemCredentialsProvider.getInstance();
 final Map<Domain, List<Credentials>> credentialsMap = credentialsProvider.getDomainCredentialsMap();
@@ -21,7 +24,14 @@ final Domain domain = Domain.global();
 if (credentialsMap.get(domain) == null) {
 	credentialsMap.put(domain, Collections.EMPTY_LIST);
 }
-credentialsMap.get(domain).add(sauceCredentials);
+
+for (Map.Entry<String, String> entry : restEndpoints.entrySet()) {
+
+	SauceCredentials sauceCredentials = new SauceCredentials(CredentialsScope.GLOBAL, entry.key,SAUCE_USERNAME, SAUCE_ACCESS_KEY, entry.value, "Created by Dockerfile. " + entry.key);
+	credentialsMap.get(domain).add(sauceCredentials);
+
+}
+
 
 credentialsProvider.setDomainCredentialsMap(credentialsMap);
 credentialsProvider.save();
