0e3ac7fc40f2d089a209151fabbe8937ff259bc7
Enrique Gonzalez <enrique@saucelabs.com>
2019-09-03 12:46:12 -0700

    add static DC

--- groovy/createSauceCredentials.groovy
+++ groovy/createSauceCredentials.groovy
@@ -7,8 +7,11 @@ import com.cloudbees.plugins.credentials.domains.Domain;
 String SAUCE_USERNAME = System.getenv("DOCKER_SAUCE_USERNAME");
 String SAUCE_ACCESS_KEY = System.getenv("DOCKER_SAUCE_ACCESS_KEY");
 
+//TODO: Make this dynamic
+String restEndpoint = "https://saucelabs.com/";
+
 // String createdCredentialId = UUID.randomUUID().toString();
-SauceCredentials sauceCredentials = new SauceCredentials(CredentialsScope.GLOBAL, "saucelabs",SAUCE_USERNAME, SAUCE_ACCESS_KEY, "Created by Dockerfile");
+SauceCredentials sauceCredentials = new SauceCredentials(CredentialsScope.GLOBAL, "saucelabs",SAUCE_USERNAME, SAUCE_ACCESS_KEY, restEndpoint, "Created by Dockerfile");
 
 final SystemCredentialsProvider credentialsProvider = SystemCredentialsProvider.getInstance();
 final Map<Domain, List<Credentials>> credentialsMap = credentialsProvider.getDomainCredentialsMap();
